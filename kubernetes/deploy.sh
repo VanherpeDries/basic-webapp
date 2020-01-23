3ec5d10ff9dfd3c075b01c6c99c1ff28220fab73
dries <dries.vanherpe@student.howest.be>
2020-01-23 14:46:04 +0100

    final

--- kubernetes/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-kubectl apply -f webapp-service.yaml
-kubectl apply -f webapp-deployment.yaml 
-kubectl apply -f nginx-deployment.yaml 
-kubectl apply -f nginx-service.yaml 
-#kubectl apply -f nginx-ingress.yaml 
-kubectl apply -f postgres-deployment.yaml 
-kubectl apply -f postgres-service.yaml 
-
